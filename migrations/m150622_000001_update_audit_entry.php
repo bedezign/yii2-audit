391f0e89ddb620548d8850dd4db09cebb8744ee1
Brett O'Donnell <cornernote@gmail.com>
2015-06-22 10:00:41 +0930

    Create m150622_000001_update_audit_entry.php

---
+++ migrations/m150622_000001_update_audit_entry.php
@@ -0,0 +1,19 @@
+<?php
+
+use yii\db\Schema;
+
+class m150622_000001_update_audit_entry extends \yii\db\Migration
+{
+    const TABLE = '{{%audit_entry}}';
+
+    public function up()
+    {
+        $this->addColumn(self::TABLE, 'request_method', 'varchar(255) NULL AFTER memory_max');
+        $this->createIndex('idx_audit_entry_request_method', self::TABLE, ['request_method']);
+    }
+
+    public function down()
+    {
+        $this->dropColumn(self::TABLE, 'memory_max');
+    }
+}
