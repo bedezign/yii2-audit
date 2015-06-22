97a5a6cfb7e49b88b2e5f4f7af41e0e5469d41e0
Brett O'Donnell <cornernote@gmail.com>
2015-06-22 14:56:59 +0930

    Update m150622_000001_update_audit_entry.php

--- migrations/m150622_000001_update_audit_entry.php
+++ migrations/m150622_000001_update_audit_entry.php
@@ -6,10 +6,22 @@ class m150622_000001_update_audit_entry extends \yii\db\Migration
 {
     const TABLE = '{{%audit_entry}}';
 
-    public function up()
+    public function safeUp()
     {
         $this->addColumn(self::TABLE, 'request_method', 'varchar(255) NULL AFTER memory_max');
         $this->createIndex('idx_audit_entry_request_method', self::TABLE, ['request_method']);
+
+        $query = \bedezign\yii2\audit\models\AuditEntry::find();
+        foreach ($query->batch() as $auditEntries) {
+            foreach ($auditEntries as $auditEntry) {
+                $auditEntry->request_method = \yii\helpers\ArrayHelper::getValue($auditEntry->data, 'env.REQUEST_METHOD');
+                if (!$auditEntry->request_method) {
+                    $auditEntry->request_method = 'CLI';
+                }
+                $auditEntry->save(false, ['request_method']);
+            }
+        }
+
     }
 
     public function down()
