3f171231d43e7c086a089b80a11e5e2c670e722e
cornernote <cornernote@gmail.com>
2015-06-23 00:21:36 +0930

    trigger new build

--- tests/unit/TestTest.php
+++ tests/unit/TestTest.php
@@ -6,16 +6,11 @@ use PHPUnit_Extensions_Database_DataSet_QueryDataSet;
 use tests\models\Post;
 use Yii;
 
-/**
- * TestTest
- */
 class TestTest extends DatabaseTestCase
 {
-
     public function testAttributeOrder()
     {
         $post = new Post();
         $this->assertEquals($post->attributes(), ['id', 'title', 'body']);
     }
-
 }
