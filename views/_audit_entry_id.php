f421e41111ca7169f0856511dca0443f2ec83e6b
Brett O'Donnell <cornernote@gmail.com>
2015-06-22 12:40:25 +0930

    Update _audit_entry_id.php

--- views/_audit_entry_id.php
+++ views/_audit_entry_id.php
@@ -5,13 +5,14 @@ use yii\helpers\Html;
 
 /** @var yii\web\View $this */
 /** @var bedezign\yii2\audit\models\AuditEntry $entry */
+/** @var bool $link */
 
 if ($auditEntry = Auditing::current()->getEntry()) {
     $style = YII_DEBUG ? '' : 'color:transparent;';
-
-    // link
-    //echo Html::a('audit-' . $auditEntry->id, ['/auditing/default/view', 'id' => $auditEntry->id], ['style' => $style]);
-
-    // span
-    echo Html::tag('span', 'audit-' . $auditEntry->id, ['style' => $style]);
+    $link = isset($link) && $link ? $link : false;
+    if ($link) {
+        echo Html::a('audit-' . $auditEntry->id, ['/auditing/default/view', 'id' => $auditEntry->id], ['style' => $style]);
+    } else {
+        echo Html::tag('span', 'audit-' . $auditEntry->id, ['style' => $style]);
+    }
 }
