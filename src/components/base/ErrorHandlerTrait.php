e637d945a7ea33814dc1cb045d739474e45f847c
cornernote <cornernote@gmail.com>
2015-06-23 19:13:14 +0930

    cleanup error handler

--- src/components/base/ErrorHandlerTrait.php
+++ src/components/base/ErrorHandlerTrait.php
@@ -1,29 +1,45 @@
 <?php
 /**
- * Error Handler as a replacement for the existing one.
+ * Error Handler allows errors to be logged to the audit_error table.
  */
 
 namespace bedezign\yii2\audit\components\base;
 
 use bedezign\yii2\audit\Audit;
 use bedezign\yii2\audit\models\AuditError;
+use Exception;
+use Yii;
 
+/**
+ * ErrorHandlerTrait
+ * @package bedezign\yii2\audit\components\base
+ */
 trait ErrorHandlerTrait
 {
+    /**
+     * @param Exception $exception
+     */
     public function logException($exception)
     {
         try {
-            $audit = Audit::current();
+
             // Make sure not to interfere with out of memory errors, there's not enough spare room to load everything
-            if ($audit && strncmp($exception->getMessage(), 'Allowed memory size of', 22)) {
+            if (strncmp($exception->getMessage(), 'Allowed memory size of', 22) == 0) {
+                return;
+            }
+
+            /** @var Audit $audit */
+            $audit = Yii::$app->getModule('audit');
+            if ($audit) {
                 $entry = $audit->getEntry(true);
                 if ($entry) {
                     AuditError::log($entry, $exception);
                     $entry->finalize();
                 }
             }
+
+        } catch (\Exception $e) {
         }
-        catch (\Exception $e) {}
 
         parent::logException($exception);
     }
