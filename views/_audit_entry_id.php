7322d8d03628af7952115c10917da39fde8163f2
Brett O'Donnell <cornernote@gmail.com>
2015-06-23 09:39:50 +0930

    Update _audit_entry_id.php

--- views/_audit_entry_id.php
+++ views/_audit_entry_id.php
@@ -2,15 +2,14 @@
 
 use bedezign\yii2\audit\Auditing;
 use yii\helpers\Html;
+use Yii;
 
 /** @var yii\web\View $this */
 /** @var bedezign\yii2\audit\models\AuditEntry $entry */
-/** @var bool $link */
 
 if ($auditEntry = Auditing::current()->getEntry()) {
     $style = YII_DEBUG ? '' : 'color:transparent;';
-    $link = isset($link) && $link ? $link : false;
-    if ($link) {
+    if (Yii::$app->auditing->checkAccess()) {
         echo Html::a('audit-' . $auditEntry->id, ['/auditing/default/view', 'id' => $auditEntry->id], ['style' => $style]);
     } else {
         echo Html::tag('span', 'audit-' . $auditEntry->id, ['style' => $style]);
