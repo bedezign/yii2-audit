3c340db992c4ad2fca1e052556ae6bf7d36156ce
cornernote <cornernote@gmail.com>
2015-06-23 19:17:23 +0930

    cleanup docblocks

--- src/Audit.php
+++ src/Audit.php
@@ -44,29 +44,45 @@ class Audit extends \yii\base\Module
      */
     public $layout = 'main';
 
-    /** @var string             name of the component to use for database access  */
-    public $db                  = 'db';
+    /**
+     * @var string name of the component to use for database access
+     */
+    public $db = 'db';
 
-    /** @var string[]           List of actions to track. '*' is allowed as the last character to use as wildcard. */
-    public $trackActions        = ['*'];
+    /**
+     * @var string|string[] List of actions to track. '*' is allowed as the last character to use as wildcard.
+     */
+    public $trackActions = ['*'];
 
-    /** @var string[]           Actions to ignore. '*' is allowed as the last character to use as wildcard (eg 'debug/*'). */
-    public $ignoreActions       = [];
+    /**
+     * @var string|string[] Actions to ignore. '*' is allowed as the last character to use as wildcard (eg 'debug/*').
+     */
+    public $ignoreActions = [];
 
-    /** @var int                Maximum age (in days) of the audit entries before they are truncated */
-    public $maxAge              = null;
+    /**
+     * @var int Maximum age (in days) of the audit entries before they are truncated
+     */
+    public $maxAge = null;
 
-    /** @var int[]              (List of) user(s) IDs with access to the viewer, null for everyone (if the role matches) */
-    public $accessUsers         = null;
+    /**
+     * @var int|int[] (List of) user(s) IDs with access to the viewer, null for everyone (if the role matches)
+     */
+    public $accessUsers = null;
 
-    /** @var string[]           (List of) role(s) with access to the viewer, null for everyone (if the user matches) */
-    public $accessRoles         = 'admin';
+    /**
+     * @var string|string[] (List of) role(s) with access to the viewer, null for everyone (if the user matches)
+     */
+    public $accessRoles = 'admin';
 
-    /** @var bool               Compress extra data generated or just keep in text? For people who don't like binary data in the DB */
-    public $compressData        = true;
+    /**
+     * @var bool Compress extra data generated or just keep in text? For people who don't like binary data in the DB
+     */
+    public $compressData = true;
 
-    /** @var array              List of providers that will capture and display data */
-    public $providers           = [
+    /**
+     * @var array List of providers that will capture and display data
+     */
+    public $providers = [
         [
             'class' => 'bedezign\yii2\audit\providers\SuperGlobalProvider',
             'logVars' => [
@@ -91,14 +107,20 @@ class Audit extends \yii\base\Module
         //'bedezign\yii2\audit\providers\EmailProvider',
     ];
 
-    /** @var array              List of initialized providers */
-    private $_providers         = [];
+    /**
+     * @var array List of initialized providers
+     */
+    private $_providers = [];
 
-    /** @var static             The current instance */
-    private static $_current    = null;
+    /**
+     * @var static The current instance
+     */
+    private static $_current = null;
 
-    /** @var \bedezign\yii2\audit\models\AuditEntry If activated this is the active entry*/
-    private $_entry             = null;
+    /**
+     * @var \bedezign\yii2\audit\models\AuditEntry If activated this is the active entry
+     */
+    private $_entry = null;
 
     public function init()
     {
@@ -107,7 +129,7 @@ class Audit extends \yii\base\Module
         parent::init();
 
         // Allow the users to specify a simple string if there is only 1 entry
-        $this->trackActions  = ArrayHelper::toArray($this->trackActions);
+        $this->trackActions = ArrayHelper::toArray($this->trackActions);
         $this->ignoreActions = ArrayHelper::toArray($this->ignoreActions);
 
         if ($this->accessRoles)
@@ -153,9 +175,9 @@ class Audit extends \yii\base\Module
 
     /**
      * Associate extra data with the current entry (if any)
-     * @param string    $name
-     * @param mixed     $data       The data to associate with the current entry
-     * @param string    $type       Optional type argument
+     * @param string $name
+     * @param mixed $data The data to associate with the current entry
+     * @param string $type Optional type argument
      * @return \bedezign\yii2\audit\models\AuditData
      */
     public function data($name, $data, $type = null)
@@ -192,9 +214,11 @@ class Audit extends \yii\base\Module
 
         if ($this->accessRoles) {
             foreach ($this->accessRoles as $role) {
-                if ($role === '?') { if ($user->getIsGuest()) return true; }
-                elseif ($role === '@') { if (!$user->getIsGuest()) return true; }
-                elseif ($user->can($role)) return true;
+                if ($role === '?') {
+                    if ($user->getIsGuest()) return true;
+                } elseif ($role === '@') {
+                    if (!$user->getIsGuest()) return true;
+                } elseif ($user->can($role)) return true;
             }
         }
 
@@ -221,7 +245,7 @@ class Audit extends \yii\base\Module
             };
         }
 
-        return [ 'class' => \yii\filters\AccessControl::className(), 'rules' => [ $rule ] ];
+        return ['class' => \yii\filters\AccessControl::className(), 'rules' => [$rule]];
     }
 
     /**
@@ -249,8 +273,8 @@ class Audit extends \yii\base\Module
      * Entries in the list are allowed to end with a '*', which means that a substring will be used for the match
      * instead of a full compare.
      *
-     * @param string    $route      An application rout
-     * @param string[]  $list       List of routes to compare against.
+     * @param string $route An application rout
+     * @param string[] $list List of routes to compare against.
      * @return bool
      */
     protected function routeMatches($route, $list)
@@ -278,7 +302,7 @@ class Audit extends \yii\base\Module
             return;
         }
         foreach ($this->providers AS $class) {
-            $provider           = Yii::createObject($class);
+            $provider = Yii::createObject($class);
             $this->_providers[] = $provider;
             Yii::trace("Initialized audit provider '{" . get_class($provider) . "}'", 'audit');
         }
