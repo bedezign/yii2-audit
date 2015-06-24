67e5ebc8ea65c54b4918ea221f976677d80eb2f4
cornernote <cornernote@gmail.com>
2015-06-25 01:03:52 +0930

    tweak include path for Kernel

--- tests/codeception/_bootstrap.php
+++ tests/codeception/_bootstrap.php
@@ -6,16 +6,16 @@ require __DIR__ . '/_init.php';
 
 $kernel = Kernel::getInstance();
 $kernel->init([
-    'debug'        => true,
-    'includePaths' => [__DIR__ . '/../../', VENDOR_DIR],
+    'debug' => true,
+    'includePaths' => [__DIR__ . '/../../src', VENDOR_DIR],
     'excludePaths' => [__DIR__],
-    'cacheDir'     => '/tmp/yii2-audit/aop',
+    'cacheDir' => '/tmp/yii2-audit/aop',
 ]);
 $kernel->loadFile(VENDOR_DIR . '/yiisoft/yii2/Yii.php');
 
 $_SERVER['SCRIPT_FILENAME'] = YII_TEST_ENTRY_FILE;
-$_SERVER['SCRIPT_NAME']     = YII_TEST_ENTRY_URL;
-$_SERVER['SERVER_NAME']     = 'localhost';
+$_SERVER['SCRIPT_NAME'] = YII_TEST_ENTRY_URL;
+$_SERVER['SERVER_NAME'] = 'localhost';
 
 Yii::setAlias('@tests', dirname(__DIR__));
 Yii::setAlias('@bedezign/yii2/audit', realpath(__DIR__ . '../../src'));
