87ab97798e9311aed2478ea11831b0772a2c4d58
cornernote <cornernote@gmail.com>
2015-06-25 02:53:40 +0930

    add code coverage on success and logs on error

--- tests/codeception/_app/config/console.php
+++ tests/codeception/_app/config/console.php
@@ -4,7 +4,18 @@ return [
     'id' => 'yii2-audit-console',
     'basePath' => dirname(__DIR__),
     'components' => [
-        'log' => null,
+        'log' => [
+            'traceLevel' => getenv('YII_TRACE_LEVEL'),
+            'targets' => [
+                [
+                    'class' => 'yii\log\FileTarget',
+                    'levels' => ['error', 'warning', 'info'],
+                    'logVars' => ['_GET', '_POST', '_FILES', '_COOKIE', '_SESSION'],
+                    'logFile' => '@app/runtime/logs/web.log',
+                    'dirMode' => 0777
+                ],
+            ],
+        ],
         'cache' => null,
         'db' => require __DIR__ . '/db.php',
     ],
--- tests/codeception/_app/config/web.php
+++ tests/codeception/_app/config/web.php
@@ -31,7 +31,18 @@ $config = [
         ],
         'cache' => null,
         'db' => require __DIR__ . '/db.php',
-        'log' => null,
+        'log' => [
+            'traceLevel' => getenv('YII_TRACE_LEVEL'),
+            'targets' => [
+                [
+                    'class' => 'yii\log\FileTarget',
+                    'levels' => ['error', 'warning', 'info'],
+                    'logVars' => ['_GET', '_POST', '_FILES', '_COOKIE', '_SESSION'],
+                    'logFile' => '@app/runtime/logs/web.log',
+                    'dirMode' => 0777
+                ],
+            ],
+        ],
         'mailer' => [
             'class' => 'yii\swiftmailer\Mailer',
             'useFileTransport' => true
