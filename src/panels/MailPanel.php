bebadb7153cde69492e1740d74f59b2434b86ba6
cornernote <cornernote@gmail.com>
2015-06-24 12:11:33 +0930

    fix mail filtering

--- src/panels/MailPanel.php
+++ src/panels/MailPanel.php
@@ -12,7 +12,7 @@ class MailPanel extends \yii\debug\panels\MailPanel
         $searchModel = new Mail();
         $dataProvider = $searchModel->search(Yii::$app->request->get(), $this->data);
 
-        return Yii::$app->view->render('@yii/debug/views/default/panels/mail/detail', [
+        return Yii::$app->view->render('panels/mail/detail', [
             'panel' => $this,
             'dataProvider' => $dataProvider,
             'searchModel' => $searchModel
