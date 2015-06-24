f31b3f7735f577448d7b63aed49ac5c7f774a569
cornernote <cornernote@gmail.com>
2015-06-24 14:58:52 +0930

    use getEvent() instead of event, import classes, add docblocks

--- src/assets/JSLoggingAsset.php
+++ src/assets/JSLoggingAsset.php
@@ -5,30 +5,50 @@
 
 namespace bedezign\yii2\audit\assets;
 
-class JSLoggingAsset extends \yii\web\AssetBundle
+use bedezign\yii2\audit\Audit;
+use yii\web\AssetBundle;
+use yii\web\View;
+
+/**
+ * JSLoggingAsset
+ * @package bedezign\yii2\audit\assets
+ */
+class JSLoggingAsset extends AssetBundle
 {
+    /**
+     * @var string
+     */
     public $sourcePath = '@bedezign/yii2/audit/assets';
 
+    /**
+     * @var array
+     */
     public $js = [
         'javascript/logger.js',
     ];
 
+    /**
+     *
+     */
     public function init()
     {
         // Activate the logging as soon as we can
-        $this->jsOptions['position'] = \yii\web\View::POS_HEAD;
+        $this->jsOptions['position'] = View::POS_HEAD;
         parent::init();
     }
 
+    /**
+     * @param \yii\web\AssetManager $assetManager
+     */
     public function publish($assetManager)
     {
-        $module = \bedezign\yii2\audit\Audit::current();
-        if ($module && $module->entry) {
+        $module = Audit::current();
+        if ($module && $module->getEntry()) {
             // We can't be sure that the actual logger was loaded already, so we fallback on the window object
             // to store the associated audit url and entry id
             $url = \yii\helpers\Url::to(["/{$module->id}/javascript/log"]);
             $id = $module->getEntry()->id;
-            \Yii::$app->view->registerJs("window.auditUrl = '$url'; window.auditEntry = $id;", \yii\web\View::POS_HEAD);
+            \Yii::$app->view->registerJs("window.auditUrl = '$url'; window.auditEntry = $id;", View::POS_HEAD);
         }
 
         return parent::publish($assetManager);
