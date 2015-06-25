adc4eb72afbf0595c950834616963b456afe9370
cornernote <cornernote@gmail.com>
2015-06-26 01:33:34 +0930

    summary in entry view, fixes #73

--- src/panels/RequestPanel.php
+++ src/panels/RequestPanel.php
@@ -16,6 +16,14 @@ class RequestPanel extends \yii\debug\panels\RequestPanel
     /**
      * @return string
      */
+    public function getName()
+    {
+        return parent::getName();
+    }
+
+    /**
+     * @return string
+     */
     public function getDetail()
     {
         return \Yii::$app->view->render('@yii/debug/views/default/panels/request/detail', ['panel' => $this]);
