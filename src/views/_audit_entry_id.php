4d83cbe262fa12091d329f69002c3c2660e33ccf
cornernote <cornernote@gmail.com>
2015-06-26 16:53:47 +0930

    fix footer view

--- src/views/_audit_entry_id.php
+++ src/views/_audit_entry_id.php
@@ -2,14 +2,13 @@
 
 use bedezign\yii2\audit\Audit;
 use yii\helpers\Html;
-use Yii;
 
 /** @var yii\web\View $this */
 /** @var bedezign\yii2\audit\models\AuditEntry $entry */
 
 if ($auditEntry = Audit::current()->getEntry()) {
     $style = YII_DEBUG ? '' : 'color:transparent;';
-    if (Yii::$app->audit->checkAccess()) {
+    if (Audit::current()->checkAccess()) {
         echo Html::a('audit-' . $auditEntry->id, ['/audit/default/view', 'id' => $auditEntry->id], ['style' => $style]);
     } else {
         echo Html::tag('span', 'audit-' . $auditEntry->id, ['style' => $style]);
