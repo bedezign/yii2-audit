56cbe086a418848d8b47964eaabce5565f5b74af
cornernote <cornernote@gmail.com>
2015-06-26 13:54:39 +0930

    improve entry view

--- src/views/entry/view.php
+++ src/views/entry/view.php
@@ -23,73 +23,97 @@ $this->params['breadcrumbs'][] = '#' . $model->id;
 
 <?= Html::tag('h1', $this->title) ?>
 
-<div class="row">
-    <div class="col-md-12">
-<?php
-    echo Html::tag('h2', Yii::t('audit', 'Request'), ['id' => 'entry', 'class' => 'hashtag']);
-
-    if ($model->request_method == 'CLI') {
-        $attributes = [
-            'id',
-            [
-                'label' => $model->getAttributeLabel('user_id'),
-                'value' => Audit::current()->getUserIdentifier($model->user_id),
-            ],
-            'route',
-            'request_method',
-            'url',
-            ['attribute' => 'duration', 'format' => 'decimal'],
-            ['attribute' => 'memory_max', 'format' => 'shortsize'],
-            'created',
-        ];
-    } else {
-        $attributes = [
-            'id',
-            [
-                'label' => $model->getAttributeLabel('user_id'),
-                'value' => Audit::current()->getUserIdentifier($model->user_id),
-            ],
-            'ip',
-            'route',
-            'request_method',
-            [
-                'label' => $model->getAttributeLabel('ajax'),
-                'value' => $model->ajax ? Yii::t('audit', 'Yes') : Yii::t('audit', 'No'),
-            ],
-            'url',
-            'referrer',
-            'redirect',
-            ['attribute' => 'duration', 'format' => 'decimal'],
-            ['attribute' => 'memory_max', 'format' => 'shortsize'],
-            'created',
-        ];
-    }
-
-    echo DetailView::widget([
-        'model' => $model,
-        'attributes' => $attributes
-    ]);
-?>
-
-<?php \yii\widgets\Pjax::begin(['id'=>'audit-panels','timeout'=>5000]); ?>
-<form>
     <div class="row">
-        <div class="col-md-2">
-            <div class="list-group">
-                <?php
-                foreach ($panels as $id => $panel) {
-                    $label = '<i class="glyphicon glyphicon-chevron-right"></i>' . $panel->getLabel();
-                    echo Html::a($label, ['view', 'id' => $model->id, 'panel' => $id], [
-                        'class' => $panel === $activePanel ? 'list-group-item active' : 'list-group-item',
-                    ]);
-                }
-                ?>
-            </div>
+        <div class="col-md-4">
+            <?php
+            echo Html::tag('h2', Yii::t('audit', 'Request'), ['id' => 'entry', 'class' => 'hashtag']);
+
+            if ($model->request_method == 'CLI') {
+                $attributes = [
+                    'route',
+                    'request_method',
+                ];
+            } else {
+                $attributes = [
+                    [
+                        'label' => $model->getAttributeLabel('user_id'),
+                        'value' => Audit::current()->getUserIdentifier($model->user_id),
+                    ],
+                    'ip',
+                    'route',
+                    'request_method',
+                    [
+                        'label' => $model->getAttributeLabel('ajax'),
+                        'value' => $model->ajax ? Yii::t('audit', 'Yes') : Yii::t('audit', 'No'),
+                    ],
+                ];
+            }
+
+            echo DetailView::widget([
+                'model' => $model,
+                'attributes' => $attributes
+            ]);
+            ?>
         </div>
-        <div class="col-md-10">
-            <?php if ($activePanel) echo $activePanel->getDetail(); ?>
-            <input type="hidden" name="panel" value="<?= $activePanel->id ?>" />
+        <div class="col-md-4">
+            <?php
+            echo Html::tag('h2', Yii::t('audit', 'URL'), ['class' => 'hashtag']);
+
+            if ($model->request_method == 'CLI') {
+                $attributes = [
+                    'url',
+                ];
+            } else {
+                $attributes = [
+                    'url',
+                    'referrer',
+                    'redirect',
+                ];
+            }
+
+            echo DetailView::widget([
+                'model' => $model,
+                'attributes' => $attributes
+            ]);
+            ?>
+        </div>
+        <div class="col-md-4">
+            <?php
+            echo Html::tag('h2', Yii::t('audit', 'Profiling'), ['id' => 'entry', 'class' => 'hashtag']);
+
+            $attributes = [
+                ['attribute' => 'duration', 'format' => 'decimal'],
+                ['attribute' => 'memory_max', 'format' => 'shortsize'],
+                'created',
+            ];
+
+            echo DetailView::widget([
+                'model' => $model,
+                'attributes' => $attributes
+            ]);
+            ?>
         </div>
     </div>
-</form>
+
+<?php \yii\widgets\Pjax::begin(['id' => 'audit-panels', 'timeout' => 5000]); ?>
+    <form>
+        <div class="row">
+            <div class="col-md-2">
+                <div class="list-group">
+                    <?php
+                    foreach ($panels as $id => $panel) {
+                        $label = '<i class="glyphicon glyphicon-chevron-right"></i>' . $panel->getLabel();
+                        echo Html::a($label, ['view', 'id' => $model->id, 'panel' => $id], [
+                            'class' => $panel === $activePanel ? 'list-group-item active' : 'list-group-item',
+                        ]);
+                    }
+                    ?>
+                </div>
+            </div>
+            <div class="col-md-10">
+                <?php if ($activePanel) echo $activePanel->getDetail(); ?>
+                <input type="hidden" name="panel" value="<?= $activePanel->id ?>"/>
+            </div>
+        </div>
+    </form>
 <?php \yii\widgets\Pjax::end(); ?>
