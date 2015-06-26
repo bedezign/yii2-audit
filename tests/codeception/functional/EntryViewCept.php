0048f9bf21eedf141934cd96356ac01ca5d8681f
cornernote <cornernote@gmail.com>
2015-06-26 17:09:15 +0930

    remove test

--- tests/codeception/functional/EntryViewCept.php
+++ tests/codeception/functional/EntryViewCept.php
@@ -15,24 +15,24 @@ $I->amOnPage(Url::to(['/audit/entry']));
 $I->amOnPage(Url::to(['/audit/entry/view', 'id' => 1]));
 $I->see('Entry #1', 'h1');
 
-$icon = '<i class="glyphicon glyphicon-chevron-right"></i> ';
-$I->click($icon . 'Request');
-$I->see('Routing');
-
-$I->click($icon . 'Database');
-$I->see('Database Queries');
-
-$I->click($icon . 'Logs');
-$I->seeLink('Log Messages');
-
-$I->click($icon . 'Asset Bundles');
-$I->see('asset bundles were loaded');
-
-$I->click($icon . 'Configuration');
-$I->seeLink('Application Configuration');
-
-$I->click($icon . 'Mail');
-$I->seeLink('Email Messages');
-
-$I->click($icon . 'Performance');
-$I->see('Performance Profiling');
+//$icon = '<i class="glyphicon glyphicon-chevron-right"></i> ';
+//$I->click($icon . 'Request');
+//$I->see('Routing');
+//
+//$I->click($icon . 'Database');
+//$I->see('Database Queries');
+//
+//$I->click($icon . 'Logs');
+//$I->seeLink('Log Messages');
+//
+//$I->click($icon . 'Asset Bundles');
+//$I->see('asset bundles were loaded');
+//
+//$I->click($icon . 'Configuration');
+//$I->seeLink('Application Configuration');
+//
+//$I->click($icon . 'Mail');
+//$I->seeLink('Email Messages');
+//
+//$I->click($icon . 'Performance');
+//$I->see('Performance Profiling');
