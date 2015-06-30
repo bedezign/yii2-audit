fe074392ba64c5d741758bb9686c07902fc13a5a
Steve Guns <Steve@bedezign.com>
2015-06-30 09:42:20 +0200

    Error message field changed to "TEXT"

--- src/migrations/m150626_000003_create_audit_error.php
+++ src/migrations/m150626_000003_create_audit_error.php
@@ -12,7 +12,7 @@ class m150626_000003_create_audit_error extends \yii\db\Migration
             'id'         => Schema::TYPE_PK,
             'entry_id'   => Schema::TYPE_INTEGER . ' NOT NULL',
             'created'    => Schema::TYPE_DATETIME . ' NOT NULL',
-            'message'    => Schema::TYPE_STRING . '(512) NOT NULL',
+            'message'    => Schema::TYPE_TEXT . ' NOT NULL',
             'code'       => Schema::TYPE_INTEGER . " DEFAULT '0'",
             'file'       => Schema::TYPE_STRING . '(512)',
             'line'       => Schema::TYPE_INTEGER ,
@@ -21,7 +21,7 @@ class m150626_000003_create_audit_error extends \yii\db\Migration
         ], ($this->db->driverName === 'mysql' ? 'CHARACTER SET utf8 COLLATE utf8_general_ci ENGINE=InnoDB' : null));
 
         $this->addForeignKey('fk_audit_error_entry_id', self::TABLE, ['entry_id'], '{{%audit_entry}}', 'id', 'CASCADE');
-        $this->createIndex('idx_message', self::TABLE, ['message']);
+        $this->createIndex('idx_message', self::TABLE, ['message(256)']);
         $this->createIndex('idx_file', self::TABLE, ['file']);
         $this->createIndex('idx_status', self::TABLE, ['status']);
     }
