4138768bfb67054c90c993fbbea366cb6d9473de
cornernote <cornernote@gmail.com>
2015-07-13 08:23:20 +0930

    allow environment to choose debug level

--- tests/codeception/_app/web/index.php
+++ tests/codeception/_app/web/index.php
@@ -1,6 +1,6 @@
 <?php
 
-defined('YII_DEBUG') or define('YII_DEBUG', true);
+defined('YII_DEBUG') or define('YII_DEBUG', getenv('YII_DEBUG') ? getenv('YII_DEBUG') : true);
 defined('YII_ENV') or define('YII_ENV', getenv('YII_ENV') ? getenv('YII_ENV') : 'test');
 defined('VENDOR_DIR') or define('VENDOR_DIR', __DIR__ . '/../../../../vendor');
 
--- tests/codeception/_init.php
+++ tests/codeception/_init.php
@@ -1,6 +1,6 @@
 <?php
 
-defined('YII_DEBUG') or define('YII_DEBUG', true);
+defined('YII_DEBUG') or define('YII_DEBUG', getenv('YII_DEBUG') ? getenv('YII_DEBUG') : true);
 defined('YII_ENV') or define('YII_ENV', getenv('YII_ENV') ? getenv('YII_ENV') : 'test');
 defined('YII_TEST_ENTRY_URL') or define('YII_TEST_ENTRY_URL', '/index.php');
 defined('YII_TEST_ENTRY_FILE') or define('YII_TEST_ENTRY_FILE', __DIR__ . '/_app/web/index.php');
