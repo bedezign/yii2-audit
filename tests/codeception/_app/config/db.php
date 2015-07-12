ae3253363a5c886f4c4bfc9005ec009e37e41e7e
cornernote <cornernote@gmail.com>
2015-07-12 17:35:39 +0930

    added database for heroku

--- tests/codeception/_app/config/db.php
+++ tests/codeception/_app/config/db.php
@@ -1,5 +1,16 @@
 <?php
 
+if (YII_ENV == 'heroku') {
+    $url = parse_url(getenv('DATABASE_URL'));
+    return [
+        'class' => 'yii\db\Connection',
+        'dsn' => 'pgsql:host=' . $url['host'] . ';dbname=' . substr($url['path'], 1),
+        'username' => $url['user'],
+        'password' => $url['pass'],
+        'charset' => 'utf8',
+    ];
+}
+
 return [
     'class' => 'yii\db\Connection',
     'dsn' => 'mysql:host=localhost;dbname=audit_test',
--- tests/codeception/_app/web/index.php
+++ tests/codeception/_app/web/index.php
@@ -1,7 +1,7 @@
 <?php
 
 defined('YII_DEBUG') or define('YII_DEBUG', true);
-defined('YII_ENV') or define('YII_ENV', 'test');
+defined('YII_ENV') or define('YII_ENV', getenv('YII_ENV') ? getenv('YII_ENV') : 'test');
 defined('VENDOR_DIR') or define('VENDOR_DIR', __DIR__ . '/../../../../vendor');
 
 require(VENDOR_DIR . '/autoload.php');
