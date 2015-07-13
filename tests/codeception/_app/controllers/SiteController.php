29fbdf3d921608c3adce96155ca8c187c703a2f4
cornernote <cornernote@gmail.com>
2015-07-13 17:23:25 +0930

    fix YII_DEBUG from env

--- tests/codeception/_app/controllers/SiteController.php
+++ tests/codeception/_app/controllers/SiteController.php
@@ -23,9 +23,6 @@ class SiteController extends Controller
         ];
     }
 
-    /**
-     * @return string
-     */
     public function actionIndex()
     {
         return $this->render('index');
