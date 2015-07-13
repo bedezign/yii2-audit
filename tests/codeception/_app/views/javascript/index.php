0e687c008c0fcd544caee4ccea884fdeb08cec8e
cornernote <cornernote@gmail.com>
2015-07-13 19:56:08 +0930

    added javascript demos

--- tests/codeception/_app/views/javascript/index.php
+++ tests/codeception/_app/views/javascript/index.php
@@ -12,11 +12,14 @@ $this->params['breadcrumbs'][] = $this->title;
     <div class="row">
         <div class="col-md-6">
             <h2><?= Yii::t('app', 'Create Javascripts') ?></h2>
-            <?= Menu::widget([
-                'items' => [
-                    //['label' => Yii::t('app', 'undefined variable'), 'url' => ['undefined-variable']],
-                ],
-            ]); ?>
+            <p>Watch your <code>console.log</code> to see the output, then check the results in the audit module.</p>
+            <ul>
+                <li><?= Html::a(Yii::t('app', 'create log'), 'javascript:window.jsLogger.log("this is a log")'); ?> (not logged on server)</li>
+                <li><?= Html::a(Yii::t('app', 'create info'), 'javascript:window.jsLogger.info("this is some info")'); ?> (not logged on server)</li>
+                <li><?= Html::a(Yii::t('app', 'create warn'), 'javascript:window.jsLogger.warn("this is a warning")'); ?></li>
+                <li><?= Html::a(Yii::t('app', 'create error'), 'javascript:window.jsLogger.error("this is an error")'); ?></li>
+                <li><?= Html::a(Yii::t('app', 'trigger js error'), 'javascript:test('); ?></li>
+            </ul>
         </div>
         <div class="col-md-6">
             <h2><?= Yii::t('app', 'View Javascripts') ?></h2>
@@ -27,3 +30,4 @@ $this->params['breadcrumbs'][] = $this->title;
     </div>
 
 </div>
+
