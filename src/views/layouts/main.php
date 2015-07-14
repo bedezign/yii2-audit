61c0186cb95de1bda3108789d33bf56d40a1824f
cornernote <cornernote@gmail.com>
2015-07-14 13:19:20 +0930

    added logo to navbar-brand

--- src/views/layouts/main.php
+++ src/views/layouts/main.php
@@ -7,10 +7,12 @@ use bedezign\yii2\audit\components\panels\Panel;
 use bedezign\yii2\audit\web\JSLoggingAsset;
 use yii\bootstrap\Nav;
 use yii\bootstrap\NavBar;
+use yii\debug\DebugAsset;
 use yii\helpers\Html;
 use yii\widgets\Breadcrumbs;
 
-yii\debug\DebugAsset::register($this);
+DebugAsset::register($this);
+JSLoggingAsset::register($this)
 ?>
 <?php $this->beginPage() ?>
 <!DOCTYPE html>
@@ -20,8 +22,7 @@ yii\debug\DebugAsset::register($this);
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <?= Html::csrfMetaTags() ?>
     <title><?= Html::encode($this->title) ?></title>
-    <?php JSLoggingAsset::register($this); ?>
-    <?php $this->registerCss('body{padding-top: 60px;}'); ?>
+    <?php $this->registerCss('body{padding-top: 60px;} .navbar-brand { background: url("data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABgAAAAYCAYAAADgdz34AAABsElEQVRIS82UwU3DQBBF5+8lR5IO0gFQAaGD0EGoAF+yu0dy804OMRUAFUAH0AEpIXQQbsnFi75kR8Y4wREJwpJlybveN//PH0OOfOHI58v/AYzH4zNjzCyEcLmP6tYKvPeDGONLCAHOuRhjXAJ4BjBJ03SxDboBsEIA96p6Xt3snJsDSPiuBFhrXwGcicgJ3wO4TtP0oQmyAVQrrAFY7cQY81oCynXv/SjGmBEUY7xS1ec65FcAHlb05o2WqWrvG8A5t4wxjowxfJYes6p+CGFY+N2ooDzMOcf9N00qyoZ9OYAe82NVHbQBlPbSSlW9rar4E0AGgBKZkiSEkFhrh8aYLpNB+fX1us/W2gTArClNO+eA0vM8vzDGvG+LYZIk3U6n88aerdfrXpZlyy8W7ZrK4mP241REqO6uur9YfxIRDuGjqo62xnQbqIghIcz6XETKrHdFZASAT9rLFF5Op1Pu2VytfhXe+36e5w8ALmqFfHDQAAypsgnSClCZ3D69Lu7FarWa0/OqlXXIXoC2/apCDgYgvEnJQQFNkIMDKhD+vhdHAewd013N/Wnt6Ao+AURRX/2e7CxYAAAAAElFTkSuQmCC") no-repeat 10px; padding-left: 40px; }'); ?>
     <?php $this->head() ?>
 </head>
 <body>
