f00ee78eb47278c963e03fef674c434000cd1f14
cornernote <cornernote@gmail.com>
2015-07-14 16:25:43 +0930

    fix controllerNamespace for test app console commands

--- tests/codeception/_app/config/console.php
+++ tests/codeception/_app/config/console.php
@@ -4,6 +4,7 @@ return [
     'id' => 'yii2-audit-console',
     'name' => 'Yii2 Audit Demo',
     'basePath' => dirname(__DIR__),
+    'controllerNamespace' => 'tests\app\commands',
     'bootstrap' => [
         'bedezign\yii2\audit\Bootstrap',
         'audit',
