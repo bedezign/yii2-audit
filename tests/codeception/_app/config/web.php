8e4e09ef4e1e63b28933768e618da9ea4fa94788
cornernote <cornernote@gmail.com>
2015-07-15 20:54:28 +0930

    remove urlmanager (testing why panels are not saving on demo)

--- tests/codeception/_app/config/web.php
+++ tests/codeception/_app/config/web.php
@@ -53,19 +53,18 @@ $config = [
             'enableCsrfValidation' => false,
             'enableCookieValidation' => false
         ],
-        'urlManager' => [
-            // *removed** - was causing panels to not be captured or displayed
-            //'enablePrettyUrl' => getenv('YII_ENV') == 'heroku' ? true : false,
-            //'showScriptName' => getenv('YII_ENV') == 'heroku' ? false : true,
-            //'rules' => [
-            //    '<module:[\w\-]+>/<controller:[\w\-]+>/<id:\d+>' => '<module>/<controller>/view',
-            //    '<module:[\w\-]+>/<controller:[\w\-]+>/<action:[\w\-]+>/<id:\d+>' => '<module>/<controller>/<action>',
-            //    '<module:[\w\-]+>/<controller:[\w\-]+>/<action:[\w\-]+>' => '<module>/<controller>/<action>',
-            //    '<controller:[\w\-]+>/<id:\d+>' => '<controller>/view',
-            //    '<controller:[\w\-]+>/<action:[\w\-]+>/<id:\d+>' => '<controller>/<action>',
-            //    '<controller:[\w\-]+>/<action:[\w\-]+>' => '<controller>/<action>',
-            //],
-        ],
+        //'urlManager' => [
+        //    'enablePrettyUrl' => getenv('YII_ENV') == 'heroku' ? true : false,
+        //    'showScriptName' => getenv('YII_ENV') == 'heroku' ? false : true,
+        //    'rules' => [
+        //        '<module:[\w\-]+>/<controller:[\w\-]+>/<id:\d+>' => '<module>/<controller>/view',
+        //        '<module:[\w\-]+>/<controller:[\w\-]+>/<action:[\w\-]+>/<id:\d+>' => '<module>/<controller>/<action>',
+        //        '<module:[\w\-]+>/<controller:[\w\-]+>/<action:[\w\-]+>' => '<module>/<controller>/<action>',
+        //        '<controller:[\w\-]+>/<id:\d+>' => '<controller>/view',
+        //        '<controller:[\w\-]+>/<action:[\w\-]+>/<id:\d+>' => '<controller>/<action>',
+        //        '<controller:[\w\-]+>/<action:[\w\-]+>' => '<controller>/<action>',
+        //    ],
+        //],
         'user' => [
             'identityClass' => 'tests\app\models\User',
         ],
