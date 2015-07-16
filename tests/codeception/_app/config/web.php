9643623cdb67746299401e66bd4b843c7c05f751
Brett O'Donnell <cornernote@gmail.com>
2015-07-16 23:52:35 +0930

    remove urlManager rules
    who's dumb idea was it to have those anyway?!

--- tests/codeception/_app/config/web.php
+++ tests/codeception/_app/config/web.php
@@ -56,14 +56,6 @@ $config = [
         'urlManager' => [
             'enablePrettyUrl' => getenv('YII_ENV') == 'heroku' ? true : false,
             'showScriptName' => getenv('YII_ENV') == 'heroku' ? false : true,
-            'rules' => [
-                '<module:[\w\-]+>/<controller:[\w\-]+>/<id:\d+>' => '<module>/<controller>/view',
-                '<module:[\w\-]+>/<controller:[\w\-]+>/<action:[\w\-]+>/<id:\d+>' => '<module>/<controller>/<action>',
-                '<module:[\w\-]+>/<controller:[\w\-]+>/<action:[\w\-]+>' => '<module>/<controller>/<action>',
-                '<controller:[\w\-]+>/<id:\d+>' => '<controller>/view',
-                '<controller:[\w\-]+>/<action:[\w\-]+>/<id:\d+>' => '<controller>/<action>',
-                '<controller:[\w\-]+>/<action:[\w\-]+>' => '<controller>/<action>',
-            ],
         ],
         'user' => [
             'identityClass' => 'tests\app\models\User',
