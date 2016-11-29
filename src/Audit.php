18ae5035d443ffd7a56774cd8e1ad5feebb7a5eb
i.badamshin <xlkurtlx@gmail.com>
2016-11-29 17:41:58 +0300

    fix debug dbPanel errors

--- src/Audit.php
+++ src/Audit.php
@@ -12,7 +12,6 @@ namespace bedezign\yii2\audit;
 
 use bedezign\yii2\audit\components\panels\Panel;
 use bedezign\yii2\audit\models\AuditEntry;
-use bedezign\yii2\audit\models\AuditError;
 use Yii;
 use yii\base\ActionEvent;
 use yii\base\Application;
@@ -152,6 +151,11 @@ class Audit extends Module
      * @var LogTarget
      */
     public $logTarget;
+
+    /**
+     * @see \yii\debug\Module::$traceLine
+     */
+    public $traceLine = \yii\debug\Module::DEFAULT_IDE_TRACELINE;
 
     /**
      * @var array
