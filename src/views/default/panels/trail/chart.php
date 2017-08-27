e4ac37d3ed712516557bbb96b0e7f7340dc396ff
Shalvah Adebayo <shalvah.adebayo@konga.com>
2017-08-27 09:28:36 +0100

    Bugfixes for the earlier implementation - array_walk doesn't always modify array keys in place - date()'s second parameter typically works with a string'

--- src/views/default/panels/trail/chart.php
+++ src/views/default/panels/trail/chart.php
@@ -5,20 +5,29 @@ use bedezign\yii2\audit\models\AuditTrail;
 use bedezign\yii2\audit\panels\TrailPanel;
 use dosamigos\chartjs\ChartJs;
 
+//initialise defaults (0 entries) for each day
 $defaults = [];
 $startDate = strtotime('-6 days');
 foreach (range(-6, 0) as $day) {
     $defaults[date('D: Y-m-d', strtotime($day . 'days'))] = 0;
 }
+
 $results = AuditTrail::find()
     ->select(["COUNT(DISTINCT id) as count", "created AS day"])
     ->where(['between', 'created',
         date('Y-m-d 00:00:00', $startDate),
         date('Y-m-d 23:59:59')])
     ->groupBy("day")->indexBy('day')->column();
-array_walk($results, function ($count, &$key) {
-    $key = date('D: Y-m-d', date_create($key));
-});
+
+// format dates properly
+$formattedData = [];
+foreach ($results as $date => $count) {
+    $date = date('D: Y-m-d', strtotime($date));
+    $formattedData[$date] = $count;
+}
+$results = $formattedData;
+
+// replace defaults with data from db where available
 $results = array_merge($defaults, $results);
 
 echo ChartJs::widget([
