8385793d113066da63d9decf56ed41be5bfc8122
Elias Luhr <e.luhr@herzogkommunikation.de>
2023-11-17 11:41:08 +0100

    Allow non integer user ids

---
+++ src/migrations/m231117_102354_alter_audit_entry.php
@@ -0,0 +1,16 @@
+<?php
+
+use yii\db\Migration;
+
+/**
+ * Class m231117_102354_alter_audit_entry
+ */
+class m231117_102354_alter_audit_entry extends Migration
+{
+    const TABLE = '{{%audit_entry}}';
+
+    public function up()
+    {
+        $this->alterColumn(self::TABLE, 'user_id', $this->string(255)->null()->defaultValue(0));
+    }
+}
